71402d5da4a60d5b841b3c2a2f011d8a3112326c
Shishir <shishir.rajoria06@gmail.com>
2023-12-04 13:51:55 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-const app = express()//here we are creating our application
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected To database MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error", err);
+  }); //inside github people can see this only has security would be provided
 
-app.listen(3000,()=>{
-    return console.log("Server is running on port 5173")
-})
+const app = express(); //here we are creating our application
+
+app.listen(3000, () => {
+  return console.log("Server is running on port 5173");
+});
