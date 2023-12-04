4ea22c95ed2c4f4b78b962d9f7ee2a29c6010541
Shishir <shishir.rajoria06@gmail.com>
2023-12-04 14:13:05 +0530

    changes done in User Model

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -16,14 +16,14 @@ const userSchema = new mongoose.Schema({
         type:String ,
         required:true,
     }
-},{timeStamp:true});//creating schema
+},{timestamps:true});//creating schema
 
 const User  =  mongoose.model('User',userSchema);//her ths 'User' shoul be capitalize and singular
 
 export default User;
 
 // unique:true this means usrname is unique for each user
-// {timeStamp:true}: this actually tells two imp. info to mongodb 
+// {timestamps:true}: this actually tells two imp. info to mongodb 
             // 1):-time of creation of the user
             //2):- time of update of the user
 // with this we can use this extra info. later for filtering out based on time            
